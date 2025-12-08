424558171654da8cc20ee8b1f34b50121cfb44bd
unknown <rikypatel1333@gmail.com>
2025-12-08 16:10:26 +0530

    [ADD] new feature and api integration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [data,setData] = useState([])
+  const [isLoading,setIsLoading] = useState(true)
+  const getPropertyData = async()=>{
+try {
+  const url = "http://headless-practice.rf.gd/wp-json/wp/v2/properties";
+ const response = await axios.get(url)
+ console.log("response",response);
+ if(response?.status === 200){
+    setIsLoading(false)
+    setData(response.data[0])
+ }
+ 
+} catch (error) {
+  throw new Error("Response not get!");
+  
+}
+  }
+  useEffect(() => {
+    getPropertyData()
+  }, []);
+  console.log("data",data);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>this is my react
+      <header className="App-header">                        
+        {
+          isLoading ? <img src={logo} className="App-logo" alt="logo" /> : <div>
+          <h2>
+          {data?.title?.rendered}
+        </h2>
+        <ul style={{listStyle:'none'}}>
+          <li style={{marginBottom:"7px"}}>Bathroom : {data?.acf?.bathroom}</li>
+          <li style={{marginBottom:"7px"}}>Bedroom :  {data?.acf?.bedroom}</li>
+          <li style={{marginBottom:"7px"}}>Price :    {data?.acf?.price}</li>
+        </ul>
         </div>
+        }                
       </header>
     </div>
   );
